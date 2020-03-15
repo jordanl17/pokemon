2edc997ac40f00bb8980f3d55dd0379d836c2062
jordanl17 <jordanl17@me.com>
2020-03-15 14:44:52 +0000

    correcting sorting for numbered fields

--- src/helpers/sorting.js
+++ src/helpers/sorting.js
@@ -1,13 +1,27 @@
 import { SORT_DIRECTION } from "../constants";
 
-const ascendingOrderSort = (A = "", B = "") => {
-  const a = String(A).toLowerCase();
-  const b = String(B).toLowerCase();
+const string = data => String(data).toLowerCase();
+const number = data => Number(data.split(" ")[0]);
 
-  if (b < a) {
+const typeConversions = {
+  name: string,
+  num: number,
+  type: string,
+  height: number,
+  weight: number,
+  weaknesses: string,
+  next_evolution: string,
+  prev_evolution: string
+};
+
+const ascendingOrderSort = (sortBy, a = "", b = "") => {
+  const typedA = typeConversions[sortBy](a);
+  const typedB = typeConversions[sortBy](b);
+
+  if (typedB < typedA) {
     return 1;
   }
-  if (b > a) {
+  if (typedB > typedA) {
     return -1;
   }
   return 0;
@@ -15,7 +29,7 @@ const ascendingOrderSort = (A = "", B = "") => {
 
 const sorter = (sortBy, direction) => ({ [sortBy]: a }, { [sortBy]: b }) =>
   direction === SORT_DIRECTION.asc
-    ? ascendingOrderSort(a, b)
-    : -ascendingOrderSort(a, b);
+    ? ascendingOrderSort(sortBy, a, b)
+    : -ascendingOrderSort(sortBy, a, b);
 
 export default sorter;
--- src/helpers/sorting.test.js
+++ src/helpers/sorting.test.js
@@ -3,26 +3,34 @@ import sorter from "./sorting";
 describe("sorting", () => {
   it("should define the sort result on a self defined list", () => {
     const list = [
-      { test: "d", thing: "stuff" },
-      { other: "thing", test: "w" },
-      { test: "c" },
+      { name: "d", thing: "stuff" },
+      { other: "thing", name: "w" },
+      { name: "c" },
       { other: "stuff" }
     ];
 
-    const ascendingSortedResult = [...list].sort(sorter("test", "asc"));
+    const ascendingSortedResult = [...list].sort(sorter("name", "asc"));
     expect(ascendingSortedResult).toEqual([
       { other: "stuff" },
-      { test: "c" },
-      { test: "d", thing: "stuff" },
-      { other: "thing", test: "w" }
+      { name: "c" },
+      { name: "d", thing: "stuff" },
+      { other: "thing", name: "w" }
     ]);
 
-    const descendingSortedResult = [...list].sort(sorter("test", "desc"));
+    const descendingSortedResult = [...list].sort(sorter("name", "desc"));
     expect(descendingSortedResult).toEqual([
-      { other: "thing", test: "w" },
-      { test: "d", thing: "stuff" },
-      { test: "c" },
+      { other: "thing", name: "w" },
+      { name: "d", thing: "stuff" },
+      { name: "c" },
       { other: "stuff" }
     ]);
+  });
+
+  it("should sort a number correctly", () => {
+    const list = [{ weight: "12.1 kg" }, {}, { weight: "111.1 kg" }];
+
+    const result = [...list].sort(sorter("weight", "desc"));
+
+    expect(result).toEqual([{ weight: "111.1 kg" }, { weight: "12.1 kg" }, {}]);
   });
 });
